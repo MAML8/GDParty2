cea25a778b54305cc9925a4c05d8abb3273099dc
Marcos Antônio <marcanantins@gmail.com>
2024-11-20 17:42:01 -0300

    Mudanças no código de filter e lightreceiver

--- scripts/Objects/LightReceiver.gd
+++ scripts/Objects/LightReceiver.gd
@@ -8,14 +8,14 @@ signal goal;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_child(2).modulate = want;
+	$ColorWant.modulate = want;
 
 func receive_light(from: Vector2, color: Color) -> void:
 	isReceiveing = color == want && to_local(from).normalized().round() == Vector2.DOWN;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	get_child(1).modulate = Color.WHITE if !isReceiveing else Color.GREEN;
+	$ParticulasdeLuz.visible = isReceiveing;
 	if isReceiveing && !countinouslyReceiveing:
 		goal.emit(true);
 	elif !isReceiveing && countinouslyReceiveing:
